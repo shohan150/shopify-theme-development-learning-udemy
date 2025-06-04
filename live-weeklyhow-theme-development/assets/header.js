f8b9fdb2e6d4b9037fd63f70c518076afb0a7a88
shohanur_rahman <shohan.ece17@gmail.com>
2025-06-04 15:00:18 +0600

    collected

---
+++ live-weeklyhow-theme-development/assets/header.js
@@ -0,0 +1,7 @@
+document.getElementById("navigationButtonOpen").addEventListener('click', () => {
+    document.getElementById("mobileMenuContainer").style.display = "block"
+})
+
+document.getElementById("navigationButtonClose").addEventListener('click', () => {
+    document.getElementById("mobileMenuContainer").style.display = "none"
+})
