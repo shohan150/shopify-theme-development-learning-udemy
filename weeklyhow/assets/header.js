d684702919ffcc142880027b8167875c18baa745
shohanur_rahman <shohan.ece17@gmail.com>
2025-07-23 02:07:33 +0600

    start fresh

--- weeklyhow/assets/header.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("navigationButtonOpen").addEventListener('click', () => {
-    document.getElementById("mobileMenuContainer").style.display = "block"
-})
-
-document.getElementById("navigationButtonClose").addEventListener('click', () => {
-    document.getElementById("mobileMenuContainer").style.display = "none"
-})
